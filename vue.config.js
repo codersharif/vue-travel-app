ce229261f4fdcec15632bd721905ddc75436aaa3
codersharif <khansharifulislam24@gmail.com>
2020-11-20 03:47:48 +0600

    up

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,13 @@
-module.exports = {
+/*module.exports = {
   pwa: {
       workboxOptions: {
         exclude: [/_redirects/]
       }
     }
+}*/
+
+module.exports = {
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/vue-travel-app/'
+    : '/'
 }
