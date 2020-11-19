4acdbcfeafbfd619da820c7852877db5dfc350b4
codersharif <khansharifulislam24@gmail.com>
2020-11-20 01:56:43 +0600

    add dist file

---
+++ dist/js/invoices.474745fb.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["invoices"],{"51b0":function(e,n,t){"use strict";t.r(n);var c=function(){var e=this,n=e.$createElement;e._self._c;return e._m(0)},i=[function(){var e=this,n=e.$createElement,t=e._self._c||n;return t("div",[t("h2",[e._v("My Invoices")])])}],r=t("2877"),s={},l=Object(r["a"])(s,c,i,!1,null,null,null);n["default"]=l.exports}}]);
+//# sourceMappingURL=invoices.474745fb.js.map
